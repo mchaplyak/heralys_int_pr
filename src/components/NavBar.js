7e92f9dd76da3881bd4c82b49940d0c880930086
mchaplyak <m.chapliak@gmail.com>
2022-10-12 15:28:08 -0400

    Initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <p>NavBar</p>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
